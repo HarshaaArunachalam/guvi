94b7394cfb05f475deb55c703d0744cb40e87fa8
HarshaaArunachalam <51322955+HarshaaArunachalam@users.noreply.github.com>
2019-06-05 22:16:59 +0530

    Update 16.py

--- code/16.py
+++ code/16.py
@@ -2,21 +2,8 @@ N,Q=input().split()
 N=int(N)
 Q=int(Q)
 for i in range(N+1,Q):
-    if i>1:
-        for j in range(2,i):
-            if(i%j)==0:
-                break
-        else:
-            print(i)
-        
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    for j in range(2,i):
+        if(i%j)==0:
+            break
+    else:
+        print(i,end=" ")
